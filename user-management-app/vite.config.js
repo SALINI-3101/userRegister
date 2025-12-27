3e7aa7cd69b34cc576feb444a5786d2b381661ec
SALINI-3101 <msalini3101@gmail.com>
2025-12-27 21:15:58 +0530

    Update config for new repository name

--- user-management-app/vite.config.js
+++ user-management-app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/salini-3101-userlist-app.github.io/',
   server: {
     proxy: {
       '/api': {
